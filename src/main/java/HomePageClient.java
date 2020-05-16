93cb97b845bf5da268731e80196a7758d392081c
Petronela Vieru <petronela.vieru@student.upt.ro>
2020-05-16 14:47:05 +0300

    SP-55 create Interface for Client HomePage

---
+++ src/main/java/HomePageClient.java
@@ -0,0 +1,31 @@
+package main.java;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class HomePageClient extends Application{
+
+        public void start(Stage stage)
+        {
+
+        }
+    // Main Method
+    public static void main(String args[])
+    {
+
+        // launch the application
+        launch(args);
+    }
+}
