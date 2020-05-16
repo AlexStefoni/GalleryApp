e2407853892efb61d153a176a85154d87dff3502
Petronela Vieru <petronela.vieru@student.upt.ro>
2020-05-16 14:48:44 +0300

    SP-55 gridPane settings

--- src/main/java/HomePageClient.java
+++ src/main/java/HomePageClient.java
@@ -20,6 +20,25 @@ public class HomePageClient extends Application{
         public void start(Stage stage)
         {
 
+            GridPane gp=new GridPane();
+            gp.setAlignment(Pos.CENTER);
+            gp.setPadding(new Insets(40,40,40,40));
+
+            // Set the horizontal gap between columns
+            gp.setHgap(10);
+
+            // Set the vertical gap between rows
+            gp.setVgap(10);
+
+
+            ColumnConstraints columnOneConstraints = new ColumnConstraints(100, 100, Double.MAX_VALUE);
+            columnOneConstraints.setHalignment(HPos.RIGHT);
+
+            // columnTwoConstraints will be applied to all the nodes placed in column two.
+            ColumnConstraints columnTwoConstrains = new ColumnConstraints(200,200, Double.MAX_VALUE);
+            columnTwoConstrains.setHgrow(Priority.ALWAYS);
+
+            gp.getColumnConstraints().addAll(columnOneConstraints, columnTwoConstrains);
         }
     // Main Method
     public static void main(String args[])
