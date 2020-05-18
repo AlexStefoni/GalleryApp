9ea3265b6810ac9f263d48ffd8ed85a8339e2120
AlexStefoni <alex.stefoni001@gmail.com>
2020-05-18 12:24:30 +0300

    SP-4 test for user or artist

--- JsonUtil/json/JsonMainTest.java
+++ JsonUtil/json/JsonMainTest.java
@@ -8,16 +8,14 @@ import java.util.ArrayList;
 public class JsonMainTest {
 
     public static void main(String[] args) {
-       JSONObject obj = JsonUtils.getJSONObjectFromFile("/user.json");
-
-        ArrayList<User> list = new ArrayList<User>();
-        JSONArray arr = obj.getJSONArray("json.User");
 
 
 
 
-     System.out.println(JsonUtils.credentialTest("alex","bob"));
+
+     System.out.println(JsonUtils.credentialTest("pipe","pepe"));
       //json.User alex=new json.User("alex","bob",true,"Alex ","acasa","04230423);
       //json.JsonUtils.registerNewUser(alex);
+        System.out.print(JsonUtils.ifArtist("pepe","pepe"));
     }
 }
