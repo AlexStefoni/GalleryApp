db8937793bde66142307e956a6f36434192135f5
Petronela Vieru <petronela.vieru@student.upt.ro>
2020-05-27 00:08:41 +0300

    SP-28 Updating HomePage for Artist

--- src/main/java/HomePageArtist.java
+++ src/main/java/HomePageArtist.java
@@ -11,22 +11,38 @@ import javafx.scene.control.SplitPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import main.java.AddArtwork;
+import json.*;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class HomePageArtist extends Application {
 
+
+    //
+    private FileInputStream topStream;
+    //
+    private FileInputStream bottomStream;
+    //
+    private FileInputStream leftStream;
+    //
+    private FileInputStream rightStream;
+    //
+    private FileInputStream centerStream;
+
     public static void main(String[] args) {
         launch(args);
     }
 
+    @Override
     public void start(Stage primaryStage) throws FileNotFoundException {
 
         SplitPane splitPane = new SplitPane();
@@ -59,7 +75,7 @@ public class HomePageArtist extends Application {
 
         leftControl.getChildren().add(btn1);
         leftControl.getChildren().add(btn2);
-       
+
         leftControl.getChildren().add(btn5);
 
         btn1.setOnMouseClicked(new EventHandler<MouseEvent>() {
@@ -90,30 +106,37 @@ public class HomePageArtist extends Application {
 
         });
 
-        FileInputStream inputstream = null;
-        inputstream = new FileInputStream("assets/pictures/unnamed.jpg");
-        Image image = new Image(inputstream);
-        ImageView imageView = new ImageView(image);
-        imageView.setX(10);
-        imageView.setY(25);
+        BorderPane photoAlbum = new BorderPane();
 
-        //setting the fit height and width of the image view
-        imageView.setFitHeight(400);
-        imageView.setFitWidth(400);
+        //file image processing for each border pane in Album
+        ////////////////////////////////////////////////
+        //photoAlbum.setTop();
 
-        //Setting the preserve ratio of the image view
-        imageView.setPreserveRatio(true);
+        try {
+            topStream = new FileInputStream("assets/pictures/1_kSQ64h9Muyqwoy9OZr26EA.jpeg");
+        } catch (FileNotFoundException e) {
+            //TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        Image top = new Image(topStream);
+        ImageView topImview = new ImageView(top);
+        topImview.setFitHeight(100);
+        topImview.setFitWidth(100);
+
+        //border structure of right split pane
+
 
 
         Button button1 = new Button();
-        button1.setGraphic(new ImageView(image));
+        button1.setGraphic(topImview);
         button1.setOnAction(new EventHandler<ActionEvent>() {
             @Override public void handle(ActionEvent e) {
 
 
                 Stage st=new Stage();
                 try {
-                    Scene scene2=new ArtworkFrame(800,500,st,image);
+                    Scene scene2=new ArtworkFrame(800,500,st,top);
                     st.setScene(scene2);
                     st.show();
                 } catch (FileNotFoundException ex) {
@@ -121,16 +144,189 @@ public class HomePageArtist extends Application {
                 }
 
 
-                System.out.println("Accepted");
+
             }
         });
 
+        GridPane topPane = new GridPane();
+        topPane.getChildren().add(button1);
+
+        ///////////////////////////////////////////////
+
+        ////////////////////////////////////////////////
+        //photoAlbum.setBottom();
+
+        try {
+            bottomStream = new FileInputStream("assets/pictures/416275.jpg");
+        } catch (FileNotFoundException e) {
+            //TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        Image bottom = new Image(bottomStream);
+        ImageView bottomImview = new ImageView(bottom);
+        bottomImview.setFitHeight(100);
+        bottomImview.setFitWidth(100);
 
 
-        rightControl.setAlignment(Pos.CENTER);
-        rightControl.getChildren().add(button1);
 
-        splitPane.getItems().addAll(leftControl, rightControl);
+        Button button2 = new Button();
+        button2.setGraphic(bottomImview);
+        button2.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+
+
+                Stage st=new Stage();
+                try {
+                    Scene scene2=new ArtworkFrame(800,500,st,bottom);
+                    st.setScene(scene2);
+                    st.show();
+                } catch (FileNotFoundException ex) {
+                    ex.printStackTrace();
+                }
+
+
+
+            }
+        });
+
+        GridPane bottomPane = new GridPane();
+        bottomPane.getChildren().add(button2);
+        ///////////////////////////////////////////////
+
+        ////////////////////////////////////////////////
+        //photoAlbum.setLeft();
+
+        try {
+            leftStream = new FileInputStream("assets/pictures/atmosphere.jpg");
+        } catch (FileNotFoundException e) {
+            //TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        Image left = new Image(leftStream);
+        ImageView leftImview = new ImageView(left);
+        leftImview.setFitHeight(100);
+        leftImview.setFitWidth(100);
+
+
+        Button button3 = new Button();
+        button3.setGraphic(leftImview);
+        button3.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+
+
+                Stage st=new Stage();
+                try {
+                    Scene scene2=new ArtworkFrame(800,500,st,left);
+                    st.setScene(scene2);
+                    st.show();
+                } catch (FileNotFoundException ex) {
+                    ex.printStackTrace();
+                }
+
+
+
+            }
+        });
+
+        GridPane leftPane = new GridPane();
+        leftPane.getChildren().add(button3);
+        ///////////////////////////////////////////////
+
+        ////////////////////////////////////////////////
+        //photoAlbum.setRight();
+
+        try {
+            rightStream = new FileInputStream("assets/pictures/download.jpg");
+        } catch (FileNotFoundException e) {
+            //TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        Image right = new Image(rightStream);
+        ImageView rightImview = new ImageView(right);
+        rightImview.setFitHeight(100);
+        rightImview.setFitWidth(100);
+
+
+        Button button4 = new Button();
+        button4.setGraphic(rightImview);
+        button4.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+
+
+                Stage st=new Stage();
+                try {
+                    Scene scene2=new ArtworkFrame(800,500,st,right);
+                    st.setScene(scene2);
+                    st.show();
+                } catch (FileNotFoundException ex) {
+                    ex.printStackTrace();
+                }
+
+
+
+            }
+        });
+
+        GridPane rightPane = new GridPane();
+        rightPane.getChildren().add(button4);
+
+        ///////////////////////////////////////////////
+
+        ////////////////////////////////////////////////
+        //photoAlbum.setCenter();
+
+        try {
+            centerStream = new FileInputStream("assets/pictures/fi1023px-Gustav_Klimt_046.jpg");
+        } catch (FileNotFoundException e) {
+            //TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        Image center = new Image(centerStream);
+        ImageView centerImview = new ImageView(center);
+        centerImview.setFitHeight(100);
+        centerImview.setFitWidth(100);
+
+
+        Button button5 = new Button();
+        button5.setGraphic(centerImview);
+        button5.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+
+
+                Stage st=new Stage();
+                try {
+                    Scene scene2=new ArtworkFrame(800,500,st,center);
+                    st.setScene(scene2);
+                    st.show();
+                } catch (FileNotFoundException ex) {
+                    ex.printStackTrace();
+                }
+
+
+
+            }
+        });
+
+        GridPane centerPane = new GridPane();
+        centerPane.getChildren().add(button5);
+        ///////////////////////////////////////////////
+
+
+        //placing image panes within border panes
+        photoAlbum.setTop(topPane);
+        photoAlbum.setBottom(bottomPane);
+        photoAlbum.setLeft(leftPane);
+        photoAlbum.setRight(rightPane);
+        photoAlbum.setCenter(centerPane);
+
+
+
+        splitPane.getItems().addAll(leftControl, photoAlbum);
+
         Scene scene=new Scene(splitPane,800,500);
         primaryStage.setScene(scene);
         primaryStage.setTitle("App");
